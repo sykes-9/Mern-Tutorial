7b99a06e842eb1a9ca0f6f1d5ecb7903df90ab1d
sykes-9 <sykessackitey9@gmail.com>
2026-08-28 12:36:22 +0000

    Remove direct Elasticsearch logging

--- backend/config/elasticsearch.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require("@elastic/elasticsearch");
-
-const client = new Client({
-  node: "http://elasticsearch:9200",
-});
-
-module.exports = client;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
-const { logger } = require("./middleware/loggerMiddleware");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -12,8 +11,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(logger);
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
